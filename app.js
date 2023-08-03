cde91b21a87c727ca5077ac276cb0fb276055002
Md. Mubasshir Naib <121809753+MubasshirNaib@users.noreply.github.com>
2023-08-03 20:50:11 +0600

    updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const collection = require('./mongo');
-const collection2 = require('./cars');
+// const collection2 = require('./cars');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
